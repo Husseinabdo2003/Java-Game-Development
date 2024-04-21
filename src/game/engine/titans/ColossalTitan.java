44a05486908b3da806ec110bf6b4385eabe89502
Hussein Abdo <104679706+Husseinabdo2003@users.noreply.github.com>
2024-04-21 19:06:10 +0200

    Error titan done

--- src/game/engine/titans/ColossalTitan.java
+++ src/game/engine/titans/ColossalTitan.java
@@ -2,17 +2,27 @@ package game.engine.titans;
 
 public class ColossalTitan extends Titan {
     public static final int TITAN_CODE = 4;
-    
-    public ColossalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed, int resourcesValue, int dangerLevel) {
+
+    public ColossalTitan(int baseHealth, int baseDamage, int heightInMeters, int distanceFromBase, int speed,
+            int resourcesValue, int dangerLevel) {
         super(baseHealth, baseDamage, heightInMeters, distanceFromBase, speed, resourcesValue, dangerLevel);
-        
+
     }
+
     @Override
-    public boolean move(){
-        super.move();
-        if(!super.move()){
-            setSpeed(getSpeed() + 1);
+    public boolean move() {
+        int currentDistance = getDistance();
+        int speed = getSpeed();
+        if (currentDistance <= 0) {
+            return true;
         }
-        return super.move();
+        currentDistance = currentDistance - speed;
+        if (hasReachedTarget() == true) {
+            currentDistance = 0;
+        } else {
+            setDistance(currentDistance);
+        }
+        setSpeed(speed + 1);
+        return hasReachedTarget();
     }
 }
